1c2a76000126f89781782a084f6eb299b82fd0c6
SimoneMondello <mondellosimo@gmail.com>
2022-05-24 11:23:16 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore_01.Models;
+using System.Diagnostics;
+
+namespace NetCore_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View("Index");
+        }
+
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,40 @@
+﻿using NetCore_01.Utils.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetCore_01.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="Il campo titolo è obbligatorio")]
+        [StringLength(20, ErrorMessage ="Il titolo non può avere più di 20 caratteri")]
+        [MoreThanOneWordValidationAttribute]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Il campo descrizione è obbligatorio")]
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "l'URL dell'immagine è obbligatoria")]
+        [Url(ErrorMessage = "Mi dispiace l'URL inserito non è valido")]
+        public string Image { get; set; }
+
+        public int Price { get; set; }
+
+        public Post()
+        {
+
+        }
+
+        public Post(string title, string description, string image, int price)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Image = image;
+            this.Price = price;
+        }
+    }
+}
